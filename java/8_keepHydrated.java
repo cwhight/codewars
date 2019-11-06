26bc418300e90ae3ae944ff315c6fcf79193b796
Charlie Whight <36269225+cwhight@users.noreply.github.com>
2019-11-06 15:24:38 +0000

    Create 8_keepHydrated.java

---
+++ java/8_keepHydrated.java
@@ -0,0 +1,13 @@
+public class KeepHydrated  {
+
+  public int Liters(double time)  {
+    
+    //Your code goes here! Hint: You should change that -1
+    int stime = (int) Math.floor(time);
+    int litres = stime / 2;
+    
+    return litres;
+  
+  }
+
+}
